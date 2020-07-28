d0cff806b1af636e5d342ce00eb7db9d9f3246e4
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 20:33:19 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,8 +3,7 @@ require 'pry'
 def square_array(array)
   counter = 0
   array.each do |number|
-    squared_number = number**2
-    array[counter] = squared_number
+    array[counter] = number**2
     counter += 1
     # binding.pry
   end
