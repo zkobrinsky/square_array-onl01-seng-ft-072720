4ca9c3539d184f83130aa749f48a4b8ad98ebd41
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 20:31:46 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  counter = 0
+  array.each do |number|
+    squared_number = number**2
+    array[counter] = squared_number
+    counter += 1
+    # binding.pry
+  end
 end
+
+# with collect method
+
+# def square_array(array)
+#   array.collect{|number| number**2}
+# end
